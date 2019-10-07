012aa8a8183c29ed6572a36510c7ec9172e5a5a4
shangranq <52607857+shangranq@users.noreply.github.com>
2019-10-07 14:52:37 -0400

    Update sort.py

--- sort.py
+++ sort.py
@@ -1,3 +1,5 @@
+from heap import heapify, build_min_heap
+
 A = [1,4,5,6,34,2,50,8,4,3,7,7,5]
 
 def quicksort(A, low, high):
@@ -13,6 +15,13 @@ def quicksort(A, low, high):
     A[sorted_count], A[high] = A[high], A[sorted_count]
     quicksort(A, low, sorted_count-1)
     quicksort(A, sorted_count+1, high)
+    
+def heapsort(A):
+    build_min_heap(A)
+    A[0], A[-1] = A[-1], A[0]
+    for i in range(len(A)-2, -1, -1):
+        heapify(A, 0, i+1)
+        A[i], A[0] = A[0], A[i]
 
 quicksort(A, 0, len(A)-1)
 print(A)
